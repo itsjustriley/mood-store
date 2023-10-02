4e88e50f26e9b08e9a614a1cb47b06f48bcc7241
itsjustriley <142259664+itsjustriley@users.noreply.github.com>
2023-10-02 09:17:39 -0300

    add items and show items functions working

--- scripts.js
+++ scripts.js
@@ -2,35 +2,44 @@ import data from './data.js'
 
 const itemsContainer = document.querySelector('#items')
 
-// loop once for the number of items in data
-// create a new div element and give it the 'item' class
-// create an image element using the appropriate image
-// append that image to the new div
-// create p elements for descriptin and price, append them
-// create add to cart button and assign it an id based on mood
-// create data attribute data-price linked to button
-for (let i = 0; i < data.length; i += 1) {
-	const newDiv = document.createElement('div');
+
+data.forEach(mood => {
+    const newDiv = document.createElement('div');
 	newDiv.className = 'item'
 	const img = document.createElement('img');
-	img.src = data[i].image
+	img.src = mood.image
 	img.width = 300
 	img.height = 300
 	newDiv.appendChild(img)
 	itemsContainer.appendChild(newDiv)
     const desc = document.createElement ('p')
-    desc.innerText = data[i].desc
+    desc.innerText = mood.desc
     newDiv.appendChild(desc)
     const price = document.createElement ('p')
-    price.innerText = data[i].price
+    price.innerText = mood.price
     newDiv.appendChild(price)
 
     const button = document.createElement('button')
-    button.id = data[i].name
-    button.dataset.price = data[i].price
+    button.id = mood.name
+    button.dataset.price = mood.price
     button.innerHTML = "Add to Cart"
     newDiv.appendChild(button)
+});
 
+// CART
+const cart = []
 
+function addItems(name, price) {
+    const item = {name: name, price: price, qty: 1}
+    cart.push(item)
 }
 
+function showItems() {
+    console.log (`You have ${cart.length} item(s) in your cart`)
+    cart.forEach(item => {
+        console.log(`${item.qty} x ${item.name} @ ${item.price} = ${item.price * item.qty}`)
+    });
+}
+
+addItems('apple', 1.99)
+showItems()
